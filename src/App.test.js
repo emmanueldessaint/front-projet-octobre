4d1a3724860ae4259298ba09b639a13fa19cbf20
emmanuel <emmanueldessaint27220@gmail.com>
2022-11-13 14:32:56 +0100

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,51 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
+import nock from 'nock';
+import axios from "axios";
+import supertest from 'supertest';
+import * as api from "./api";
 
-test('renders learn react link', () => {
+jest.mock("./api");
+
+// import renderer from 'react-test-renderer';
+
+test('boutons et affichage du tableau', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(
+    screen.getByText("Matériel")
+  ).toBeInTheDocument();
+  expect(
+    screen.getByText("Stock")
+  ).toBeInTheDocument();
+  expect(
+    screen.getByText("Ajouter un emprunt")
+  ).toBeInTheDocument();
+  expect(
+    screen.getByText("Supprimer le matériel")
+  ).toBeInTheDocument();
+  expect(
+    screen.getByText("Ajouter du matériel")
+  ).toBeInTheDocument();
+  expect(
+    screen.getByText("Ajouter / supprimer un étudiant")
+  ).toBeInTheDocument();
 });
+
+describe('expectedData', () => {
+
+  const mock = jest.fn();
+
+  it('blabla', async () => {
+    api.getMaterielsApi.mockReturnValue(42);
+    mock();
+    console.log(mock())
+    // api.getMaterielsApi.mockReturnValue({
+    //   results: [{ prenom: "Emmanuel"}]
+    // })
+    // render(<App />);
+    // await waitFor(() => {
+    //   screen.getByText('Emmanuel')
+    // })
+  })
+})
+
